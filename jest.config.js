addc5a22bf13cc8fce5debd32ea95e090675d114
Shae Smith <sbsmith86@gmail.com>
2019-06-06 11:53:51 -0400

    adding first round of tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-    testRegex: "resources/js/test/.*.test.js$"
+    testRegex: "resources/js/test/.*.test.js$",
+    setupFilesAfterEnv: [
+        "@testing-library/react/cleanup-after-each"
+        // ... other setup files ...
+    ],
+    moduleDirectories: [
+        'node_modules',
+        // the directory with the test-utils.js file
+        'utils',
+        // the root directory
+        __dirname
+    ]
 };
