2e8794a3120435d64db5828bd201898c34bd75c3
Shae Smith <sbsmith86@gmail.com>
2019-06-06 16:09:19 -0400

    Update database.php for local env

--- config/database.php
+++ config/database.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Str;
 
+if (env('APP_ENV') !== 'local') {
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+}
+
 return [
 
     /*
