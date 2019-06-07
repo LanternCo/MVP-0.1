73a028adefdfa3025fcb65bf18b06f108e766630
Shae Smith <sbsmith86@gmail.com>
2019-06-07 05:42:54 -0400

    update datebase config

--- config/database.php
+++ config/database.php
@@ -10,7 +10,14 @@ if (env('APP_ENV') !== 'local') {
     $username = $dbparts['user'];
     $password = $dbparts['pass'];
     $database = ltrim($dbparts['path'],'/');
+} else {
+    $url = env('DATABASE_URL');
+    $hostname = env('DB_HOST', '127.0.0.1');
+    $username = env('DB_USERNAME', 'forge');
+    $password = env('DB_PASSWORD', '');
+    $database = env('DB_DATABASE', 'forge');
 }
+
 
 return [
 
@@ -55,12 +62,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => $url,
+            'host' => $hostname,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
